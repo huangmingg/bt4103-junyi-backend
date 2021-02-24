7ef26b30c4568e406bd454779a2edefe222c9f54
huangminggg <huangming.low@fundingsocieties.com>
2021-02-24 19:50:14 +0800

    Init update
    Amended migration file orders (creation of additional tables are shifted to the last since it is not finalized)

--- migration/004-data_dump_contents.down.sql
+++
@@ -1,9 +0,0 @@
-UPDATE
-    contents
-SET
-    "deleted_at" = NOW()
-  , "deleted_by" = 'e0309575@u.nus.edu'
-WHERE
-    ("deleted_at" IS NULL OR "deleted_at" > NOW())
-  AND "created_by" = 'e0309575@u.nus.edu'
-  AND "updated_by" = 'e0309575@u.nus.edu';
