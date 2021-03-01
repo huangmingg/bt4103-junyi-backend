dff8273f04899ca437b98e58112f7bc04d7f9238
huangminggg <huangming.low@fundingsocieties.com>
2021-03-01 20:15:42 +0800

    Added batching for logs migration

---
+++ migration/005-data_dump_logs.down.sql
@@ -0,0 +1,10 @@
+UPDATE
+    logs
+SET
+    "deleted_at" = NOW()
+  , "deleted_by" = 'e0309575@u.nus.edu'
+WHERE
+    ("deleted_at" IS NULL OR "deleted_at" > NOW())
+  AND "created_by" = 'e0309575@u.nus.edu'
+  AND "updated_by" = 'e0309575@u.nus.edu'
+  AND "batch" = 2
