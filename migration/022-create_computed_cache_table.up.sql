ce0e94d4e2eca219b6d2f0df623e33464efe32ae
huangminggg <huangming.low@fundingsocieties.com>
2021-03-02 12:55:26 +0800

    Migration for user computed cache

---
+++ migration/022-create_computed_cache_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS computed_cache
+(
+    "id"                    BIGSERIAL PRIMARY KEY,
+    "uuid"                  BIGSERIAL,
+    "problems_attempted"    SMALLINT,
+    "exercises_attempted"   SMALLINT,
+    "avg_time_per_exercise" DECIMAL,
+    "avg_accuracy"          DECIMAL,
+    "no_upgrades"           SMALLINT,
+    "no_downgrades"         SMALLINT,
+    "avg_hint_per_attempt"  DECIMAL,
+    "avg_time_btw_problem"  DECIMAL,
+    "created_by"            VARCHAR NOT NULL,
+    "created_at"            TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_by"            VARCHAR NULL,
+    "updated_at"            TIMESTAMPTZ DEFAULT now(),
+    "deleted_by"            VARCHAR NULL,
+    "deleted_at"            TIMESTAMPTZ NULL,
+    CONSTRAINT fk_users FOREIGN KEY (uuid) REFERENCES users(id),
+);
