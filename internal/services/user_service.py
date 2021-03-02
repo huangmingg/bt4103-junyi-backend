1fa24a1c80b85a22d8f46962202ae49f2bf59e73
huangminggg <huangming.low@fundingsocieties.com>
2021-03-02 20:59:22 +0800

    Update user_service.py

--- internal/services/user_service.py
+++ internal/services/user_service.py
@@ -1,5 +1,5 @@
 from internal.repositories.user_repo import get_users, get_user
-from internal.repositories.computed_cache import get_user_cache, create_user_cache
+from internal.repositories.computed_cache_repo import get_user_cache, create_user_cache
 
 
 class UserService:
@@ -8,7 +8,7 @@ class UserService:
     def get_user(id):
         if not get_user_cache(id):
             create_user_cache(id)
-        cache = get_user_cache()
+        cache = get_user_cache(id)
         user = get_user(id)
         return {**cache, **user}
 
